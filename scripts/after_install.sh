4d95f4030c5a71766a9858046d7e2cc0b21c849e
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2022-04-13 00:32:44 -0400

    fixed scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,3 +3,6 @@ export PATH=$PATH:/usr/local/go/bin
 go version
 cd /opt/simple-reddit
 go mod download
+
+sudo mv /opt/simple-reddit/scripts/simple-reddit-backend.service /etc/systemd/system
+sudo systemctl daemon-reload
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,6 @@ cd /opt/simple-reddit
 cp /home/ubuntu/.env /opt/simple-reddit
 sudo /usr/local/go/bin/go build -o simple-reddit-build main.go  # due to permission denied error
 echo "Running server..."
-nohup ./simple-reddit-build & disown
+# nohup ./simple-reddit-build & disown  # doesn't work as it is blocking in AWS CodeDeploy
+sudo systemctl start simple-reddit-backend.service
 echo "Server is now up!"
