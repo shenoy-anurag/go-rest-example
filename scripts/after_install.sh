ddb1395a91250f5b3f06def00b69802a62d5bc8a
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2022-04-12 23:25:17 -0400

    fixed scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+export PATH=$PATH:/usr/local/go/bin
+go version
 cd /opt/simple-reddit
 go mod download
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /opt/simple-reddit
+cp ~/.env .
 go build -o simple-reddit-build main.go
 nohup ./simple-reddit-build & disown
