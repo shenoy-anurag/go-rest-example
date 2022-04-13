648db8354582f141ee8ebc4d76727c4f127b8d7a
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2022-04-12 23:34:05 -0400

    fixed scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /opt/simple-reddit
-cp ~/.env /opt/simple-reddit
+cp /home/ubuntu/.env /opt/simple-reddit
 go build -o simple-reddit-build main.go
 nohup ./simple-reddit-build & disown
