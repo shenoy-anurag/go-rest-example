b97081e99605e1b59a2b873d6471e0879d6b21a7
Anurag <19914954+shenoy-anurag@users.noreply.github.com>
2022-04-13 00:04:15 -0400

    fixed scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,6 @@
 cd /opt/simple-reddit
 cp /home/ubuntu/.env /opt/simple-reddit
 sudo /usr/local/go/bin/go build -o simple-reddit-build main.go  # due to permission denied error
-nohup ./simple-reddit-build
+echo "Running server..."
+nohup ./simple-reddit-build &; disown
+echo "Server is now up!"
